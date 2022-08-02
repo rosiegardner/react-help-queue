ddfe56e7ebdee5fbf8a89d6fdfd2a2abba8a3351
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-01 17:48:38 -0700

    move apps to components directory

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function App() {
+  const name = "Thato";
+  const name2 = "Haley";
+  return (
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>{name} and {name2}</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
